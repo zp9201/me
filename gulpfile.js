8745608bb1db9388b930bf6be0caf0b3640130bf
zp <zp9201@yeah.net>
2017-03-14 08:24:15 +0800

    移动web更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,27 @@
-/**
- * Created by acer-pc on 2017/3/13.
- */
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+// var minify = require('gulp-minify-css');
+var cssnano = require('gulp-cssnano');
+var htmlmin = require('gulp-htmlmin')
+gulp.task('script', function () {
+    gulp.src(['./js/main.js', './js/index.js'])
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
+})
+gulp.task('style', function () {
+    gulp.src(['./css/index.css', './css/style.css', './css/reset.css'])
+    // .pipe(minify('all.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('./dist'))
+})
+gulp.task('html', function () {
+    // 1.匹配到要处理的文件
+    gulp.src(['./index.html'])
+    // 2.压缩操作
+        .pipe(htmlmin({collapseWhitespace: true}))//是否压缩空白符
+        // 3.指定输出目录
+        .pipe(gulp.dest('./dist'));
+})
+
